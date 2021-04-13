2094b6679329d3c5794e0fce24e79793760b5aae
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-14 06:36:59 +0700

    refactor: create client instance

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
+
+const client = new ApolloClient({
+  link: createUploadLink({
+    uri: 'http://localhost:4000'
+  }),
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        Hello World
+      </div>
+    </ApolloProvider>
   );
 }
 
