8c38102c6174fc68874c7a4ad88b7482be9b979d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-14 15:03:21 +0700

    refactor: add a generate random string method

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+function generateRandomString(length) {
+  var result           = [];
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result.push(characters.charAt(Math.floor(Math.random() * 
+charactersLength)));
+ }
+ return result.join('');
+}
+
 const typeDefs = gql`
   type File {
     url: String!
